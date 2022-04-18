fb5bf7699dbc2b7c6057690b16cb5f305d48ac42
Vijin S <vijin.s@TA.COM>
2022-04-18 08:30:03 +0530

    Google Search Test Cases Added

---
+++ Test Suites/Google Search.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Google Search</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>4d5d51b0-7ca0-4f29-b4b3-d7005ba9d0c6</testSuiteGuid>
+   <testCaseLink>
+      <guid>2a7da29c-0dca-4863-b290-664ef2044c90</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
